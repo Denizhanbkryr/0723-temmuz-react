55a872f4ff3bd354b12a0e51a8afc450832a9074
Erhan Fırat <erhanfirat@gmail.com>
2023-09-13 09:39:27 +0300

    products page e filtreleme eklendi

--- src/pages/ProductsPage.js
+++ src/pages/ProductsPage.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   Button,
   Card,
@@ -6,6 +6,7 @@ import {
   CardSubtitle,
   CardText,
   CardTitle,
+  Input,
   Modal,
   ModalBody,
   ModalFooter,
@@ -15,42 +16,53 @@ import { Link } from "react-router-dom";
 
 const ProductsPage = ({ products }) => {
   const [showModal, setShowModal] = useState(false);
+  const [filterText, setFilterText] = useState("");
 
   const toggle = () => setShowModal(!showModal);
 
   return (
     <div>
-      <h2>Products Page</h2>
+      <h2>Products Page | filtered by: {filterText}</h2>
       <hr />
+      <Input
+        placeholder="Write to filter..."
+        className="mb-2"
+        value={filterText}
+        onChange={(e) => setFilterText(e.target.value)}
+      />
       <div className="d-flex flex-wrap">
-        {products.map((product, i) => (
-          <Card
-            className="me-2 mb-2"
-            style={{
-              width: "18rem",
-            }}
-            key={`product-card-${i}`}
-          >
-            <img alt="Sample" src={product.img} />
-            <CardBody>
-              <CardTitle tag="h5">{product.name}</CardTitle>
-              <CardSubtitle className="mb-2 text-muted" tag="h6">
-                {product.price} TL
-              </CardSubtitle>
-              <CardText>{product.description}</CardText>
-              <Button color="primary" onClick={toggle}>
-                Sepete Ekle
-              </Button>
-              <Link
-                to={"/products/" + product.id}
-                className="ml-2"
-                data-cy="incele-link"
-              >
-                İncele
-              </Link>
-            </CardBody>
-          </Card>
-        ))}
+        {products
+          .filter((p) =>
+            p.name.toLocaleLowerCase().includes(filterText.toLocaleLowerCase())
+          )
+          .map((product, i) => (
+            <Card
+              className="me-2 mb-2"
+              style={{
+                width: "18rem",
+              }}
+              key={`product-card-${i}`}
+            >
+              <img alt="Sample" src={product.img} />
+              <CardBody>
+                <CardTitle tag="h5">{product.name}</CardTitle>
+                <CardSubtitle className="mb-2 text-muted" tag="h6">
+                  {product.price} TL
+                </CardSubtitle>
+                <CardText>{product.description}</CardText>
+                <Button color="primary" onClick={toggle}>
+                  Sepete Ekle
+                </Button>
+                <Link
+                  to={"/products/" + product.id}
+                  className="ml-2"
+                  data-cy="incele-link"
+                >
+                  İncele
+                </Link>
+              </CardBody>
+            </Card>
+          ))}
       </div>
       <Modal isOpen={showModal} toggle={toggle} centered>
         <ModalHeader toggle={toggle}>Ürün Sepete Eklendi!</ModalHeader>
